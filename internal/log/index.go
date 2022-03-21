b4266d5d33e63d5a8c8b37f6928e53e1b34d39a1
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2022-03-21 10:19:52 +0300

    feat: index struct

---
+++ internal/log/index.go
@@ -0,0 +1,18 @@
+package log
+
+import (
+	"os"
+
+	"github.com/tysonmote/gommap"
+)
+
+var (
+	offWidth = 4
+	posWidth = 8
+	endWidth = offWidth + posWidth
+)
+
+type index struct {
+	file *os.File
+	mmap gommap.MMap
+}
