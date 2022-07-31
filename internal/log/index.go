4ba47e6d7c1a76161d2282633a437317409f73a4
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2022-07-31 20:48:07 +0300

    feat: minor changes log && segment

--- internal/log/index.go
+++ internal/log/index.go
@@ -7,16 +7,29 @@ import (
 )
 
 var (
-	offWidth = 4
-	posWidth = 8
-	endWidth = offWidth + posWidth
+	offWidth uint64 = 4
+	posWidth uint64 = 8
+	endWidth        = offWidth + posWidth
 )
 
 type index struct {
 	file *os.File
 	mmap gommap.MMap
+	size uint64
 }
 
-// func newIndex(f *os.File, c Config) (*index, error) {
-
-// }
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	idx.size = uint64(fi.Size())
+	if err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	); err != nil {
+		return nil, err
+	}
+}
